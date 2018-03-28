ddf24ac2742a247320dcb1db7f566fccbafc1171
Bryce Bjork <brycedbjork@gmail.com>
2018-03-28 13:18:13 -0400

    cleaning

--- archive/functions-index-old.js
+++ archive/functions-index-old.js
@@ -9,6 +9,10 @@ firebaseConfig.databaseAuthVariableOverride = {
 admin.initializeApp(firebaseConfig);
 let firestore = admin.firestore();
 
+const CoinbaseClient = require("coinbase").Client;
+const coinbaseKey = functions.config().coinbase.key;
+const coinbaseSecret = functions.config().coinbase.secret;
+
 /**
  * slack
  * @param title = title of post to slack channel
@@ -33,6 +37,150 @@ const slack = (title, subtitle = null, content) => {
 			return error;
 		});
 };
+
+// coinbase webhook
+exports.coinbase = functions.https.onRequest((request, response) => {
+	const coinbase = new CoinbaseClient({
+		apiKey: coinbaseKey,
+		apiSecret: coinbaseSecret
+	});
+
+	return new Promise((resolve, reject) => {
+		try {
+			const timestampReceived = Math.floor(Date.now() / 1000);
+			const notificationType = request.params.type;
+
+			// if new payment to address
+			if (notificationType == "wallet:addresses:new-payment") {
+				const address = request.params.data.address;
+				const amount =
+					request.params.data.additional_data.amount.amount;
+				const currency = request.params.additional_data.amount.currency;
+				const tx = request.params.additional_data.hash;
+				const coinbaseTransactionId =
+					request.params.additional_data.transaction.id;
+				const timestampCoinbaseCreated = Math.floor(
+					Date.parse(request.params.created_at) / 1000
+				);
+
+				const addressRef = "crypto." + currency + ".address";
+				const balanceRef = "crypto." + currency + ".balance";
+
+				// find splash person associated with address
+				firestore
+					.collection("people")
+					.where(addressRef, "==", address)
+					.get()
+					.then(snapshot => {
+						const person = snapshot[0].data();
+						const email = person.email;
+						const defaultCurrency = person.default_currency;
+
+						// get exchange rate from coinbase
+						const newTransaction = new Promise(
+							(resolve, reject) => {
+								coinbase.getExchangeRates(
+									{ currency: currency },
+									(error, rates) => {
+										if (error) {
+											reject(error);
+										}
+
+										const exchangeRate =
+											rates.data.rates[defaultCurrency];
+
+										// create transaction
+										firestore
+											.collection("transaction")
+											.add({
+												type: "coinbase",
+												to_id: snapshot[0].id,
+												currency: currency,
+												amount: amount,
+												relative_currency: defaultCurrency,
+												relative_amount:
+													exchangeRate * amount,
+												tx: tx,
+												fee: {
+													currency: "BTC",
+													amount: 0 // no fee when moving money in from coinbase
+												},
+												coinbase_transaction_id: coinbaseTransactionId,
+												timestamp_initiated: timestampCoinbaseCreated,
+												timestamp_completed: timestampReceived
+											})
+											.then(() => {
+												slack(
+													"chain:coinbase:createTransaction:success",
+													email,
+													amount
+												);
+											})
+											.catch(error => {
+												slack(
+													"chain:coinbase:createTransaction:failure",
+													email,
+													error.toString()
+												);
+												reject(error);
+											});
+									}
+								);
+							}
+						);
+
+						const updateBalance = new Promise((resolve, reject) => {
+							// add inbound money to existing balance
+							const oldBalance = person.crypto[currency].balance;
+							const newBalance = oldBalance + amount;
+
+							// update person's balance
+							const updateObj = {};
+							updateObj[balanceRef] = newBalance;
+							firestore
+								.collection("people")
+								.doc(snapshot[0].id)
+								.update(updateObj)
+								.then(() => {
+									slack(
+										"chain:coinbase:updateBalance:success",
+										email,
+										oldBalance + " => " + newBalance
+									);
+								})
+								.catch(error => {
+									slack(
+										"chain:coinbase:updateBalance:failure",
+										email,
+										error.toString()
+									);
+									reject(error);
+								});
+						});
+
+						Promise.all([newTransaction, updateBalance])
+							.then(() => {
+								resolve(
+									"transaction approved and balances updated"
+								);
+							})
+							.catch(error => {
+								reject(error);
+							});
+					})
+					.catch(error => {
+						slack(
+							"chain:coinbase:queryAddress:failure",
+							error.toString()
+						);
+						reject(error);
+					});
+			}
+		} catch (err) {
+			reject(error);
+		}
+	});
+});
 
 // function called on each new transaction pushed to chain
 exports.hexaNewTransaction = functions.firestore
